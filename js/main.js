fca9cfcfb4c36f82f1a4fbb19e9903c8468722f5
Nikita Ivchenko <nikitaivchenkost@mail.ru>
2021-03-24 23:02:52 +0600

    getGoods createCards renderCards was added

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const mySwiper = new Swiper('.swiper-container', {
 
 // cart
 
-const buttonCart = document.querySelector('.buthit ton-cart');
+const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
 const modalClose = document.querySelector('.modal-close');
 
@@ -47,3 +47,47 @@ const scrollLinks = document.querySelectorAll('a.scroll-link');
       });
   }
 })()
+
+//goods
+
+const more = document.querySelector('.more');
+const navigationItem = document.querySelectorAll('.navigation-item');
+const longGoodsList = document.querySelector('.long-goods-list');
+
+const getGoods = async function() {
+  const result = await fetch('db/db.json');
+  if(!result.ok) {
+    throw 'Ошибка: ' + result.status;
+  }
+  return await result.json();
+};
+
+/*getGoods().then(function (data) {
+  console.log(data);
+});*/
+
+const createCard = function (objCard) {
+  const card = document.createElement('div');
+  card.className = 'col-lg-3 col-sm-6';
+
+  card.innerHTML = `
+        <div class="goods-card">
+						<span class="label">New</span>
+						<img src="img/image-120.jpg" alt="image: Faded Beach Trousers" class="goods-image">
+						<h3 class="goods-title">Faded Beach Trousers</h3>
+						<p class="goods-description">Navy/Ochre/Black/Khaki</p>
+						<button class="button goods-card-btn add-to-cart" data-id="007">
+						<span class="button-price">$139</span>
+						</button>
+				</div>
+  `;
+  return card;
+};
+
+const renderCards = function (data) {
+  longGoodsList.textContent = '';
+  const cards = data.map(createCard)
+  document.body.classList.add('show-goods');
+};
+
+renderCards();
